0a23df03193df91fba77d8597cea09cbb83f4169
Adam Ozdemir <adamozdemir@Adams-MacBook-Pro.local>
2022-06-13 18:48:22 +0400

    last commit

--- dicer/random_dice.py
+++ dicer/random_dice.py
@@ -4,16 +4,19 @@
 from tkinter import *
 import random
 
+background_color = "#0D865D"
 #create tkinter instance
 root=Tk()
 #define geometry
-root.geometry("400x400")
-root.title("2D Dice Roller 2022")
+root.geometry("1080x720")
+root.title("2D Dice Roller")
+#background colour
+root.configure(bg=background_color)
 
 #GUI will have two basic components
 #1.Button which will trigger the rolling of dice
 #2.Dice label
-l1=Label(root,font=("Helvetica",260))
+l1=Label(root,font=("Helvetica",330))
 
 def roll():
     #create a number variable in which the list of all the ASCII characters of the string will be stored
@@ -22,9 +25,31 @@ def roll():
     #configure the label
     l1.config(text=f'{random.choice(dice)}{random.choice(dice)}')
     l1.pack()
+    #value of dice numbers
+    #unicodes and value of correspondings
+    numbers = {'\u2680':1, '\u2681':2,
+               '\u2682':3, '\u2683':4,
+               '\u2684':5, '\u2685':6}
     
-b1=Button(root,text="Roll the Dice!",foreground='blue',command=roll)
-b1.place(x=300,y=0)
+    d1 = random.choice(dice)
+    d2 = random.choice(dice)
+
+    
+    if d1 in numbers.keys():
+        d1_number = numbers[d1]
+    
+    if d2 in numbers.keys():
+        d2_number = numbers[d2]
+
+    
+    total_number.config(text=f'You Rolled:{d1_number + d2_number}')
+    
+
+b1=Button(root,text="Roll the Dice!",foreground='red',command=roll)
+b1.place(x=200,y=100)
 b1.pack()
+
+total_number = Label(root,text= '', font=("Helvetica",28))
+total_number.pack(pady=10)
 
 root.mainloop()
